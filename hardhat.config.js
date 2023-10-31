3c63b4d1e7b08767262c8642cd1fbdd92cb036a5
Al-Qa-qa <alqaqa.fighter@gmail.com>
2023-10-31 11:40:30 +0200

    init commit

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-gas-reporter");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+  },
+};
